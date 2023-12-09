9ad99907c30fc78d5cb9d78d46317efdaa97a32a
Sedat Ozveren <sedatozveren@gmail.com>
2023-12-09 23:43:04 +0300

    son version

---
+++ src/firstGithub/P01.java
@@ -0,0 +1,8 @@
+package firstGithub;
+
+public class P01 {
+
+    public static void main(String[] args) {
+        System.out.println("Bye bye Github");
+    }
+}
---
+++ src/firstGithub/P03.java
@@ -0,0 +1,9 @@
+package firstGithub;
+
+public class P03 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Son version");
+    }
+}
